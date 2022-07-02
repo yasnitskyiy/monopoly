54c6f2594b3c5c8949f2521f6dba1c345cea0e4a
yasnitskyiy <73424628+yasnitskyiy@users.noreply.github.com>
2022-07-02 17:00:30 +0300

    first template design

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import c from './App.module.css';
+import profileImg from './images/Person.jpg'
+import templateFeatures from './images/template.png'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div className={c.container}>
+            <div className={c.header}>
+                <div className={`${c.headerContent} ${c.center}`}>
+                    <div className={c.logo}>
+                        <h1>MONOPOLY UA</h1>
+                    </div>
+                    <div className={c.headerMenu}>
+                        <ul>
+                            <li className={c.game}>PLAY GAME</li>
+                            <li>FRIENDS</li>
+                            <li>STATS</li>
+                        </ul>
+                        <div className={c.dropdown}>
+                            <a href="/" className={c.showMenu}><img src={profileImg} alt="profile-img"/></a>
+                            <div className={c.dropdownContent}>
+                                <div className={c.userInfo}>
+                                    <span className={c.userName}>My nick name</span>
+                                    <img src={profileImg} alt="profile-img"/>
+                                </div>
+                                <a href="/">Profile</a>
+                                <a href="/">Settings</a>
+                                <a href="/">Sign out</a>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+            <div className={`${c.content} ${c.center}`}>
+                <div className={c.features}>
+                    <div className={c.gridItem}>
+                        <img src={templateFeatures} alt="templateFeatures"/>
+                        <h2>FREE TO PLAY</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur eveniet in nesciunt nostrum officia, ut? Animi cum eaque earum eum ipsum, laboriosam omnis praesentium reiciendis sequi sint sit tempore unde.</p>
+                    </div>
+                    <div className={c.gridItem}>
+                        <img src={templateFeatures} alt="templateFeatures"/>
+                        <h2>NEW COMMUNICATIONS</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur eveniet in nesciunt nostrum officia, ut? Animi cum eaque earum eum ipsum, laboriosam omnis praesentium reiciendis sequi sint sit tempore unde.</p>
+                    </div>
+                    <div className={c.gridItem}>
+                        <img src={templateFeatures} alt="templateFeatures"/>
+                        <h2>COMPETITION WITH OTHERS</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur eveniet in nesciunt nostrum officia, ut? Animi cum eaque earum eum ipsum, laboriosam omnis praesentium reiciendis sequi sint sit tempore unde.</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className={`${c.footer}`}>
+                <div className={`${c.center}`}>
+
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
